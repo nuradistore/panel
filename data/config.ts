87261546db150f69a78726e9a7e0cdda4f4a41a7
nuradistore <130492351+nuradistore@users.noreply.github.com>
2026-08-30 00:20:55 +0700

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -1,48 +1,55 @@
 export const pterodactylConfig = {
   domain: (process.env.PANEL_DOMAIN || "https://panelprivatemngyaanzpanel7631.buyervps.my.id").replace(/\/$/, ""),
   apiKey: process.env.PANEL_APIKEY,
-  nests: "5", 
-  nestsGame: "2", // ga usah di isi, ga perlu
-  egg: "15", 
-  eggSamp: "16", // ga usah di isi, ga perlu
-  location: "1", // location panel 
+  nests: "5",
+  nestsGame: "2",
+  egg: "15",
+  eggSamp: "16",
+  location: "1",
 }
 
 export const appConfig = {
-  whatsappGroupLink: "https://whatsapp.com/channel/0029Valq3pQHVvThh0GDrh1w", // link group
-  nameHost: "BROCK STORE", // nama host 
-  feeMin: 135, //minimal fee
-  feeMax: 136, // max fee 
+  whatsappGroupLink: "https://whatsapp.com/channel/0029Valq3pQHVvThh0GDrh1w",
+  nameHost: "BROCK STORE",
+
+  feeMin: 135,
+  feeMax: 136,
+
   garansi: {
-    warrantyDays: 30, // Limit hari
-    replaceLimit: 5, // Limit replace/claim
+    warrantyDays: 30,
+    replaceLimit: 5,
   },
+
   pay: {
     api_key: process.env.SAKURUPIAH_APIKEY,
     api_id: process.env.SAKURUPIAH_ID,
   },
+
   emailSender: {
-    host: "brockstore71@gmail.com", // Gmail host
-    port: 587, // ga usa di ubah, ga guna 
-    secure: false, // false in
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
-      user: "brockstore71@gmail.com", // Gmail buat ngirim ke Gmail buyer 
-      pass: process.env.GMAIL_PASSWORD, // sandi aplikasi 
+      user: "brockstore71@gmail.com",
+      pass: process.env.GMAIL_PASSWORD,
     },
     from: "TIM BROCK STORE <brockstore71@gmail.com>",
-  }, // ganti sendiri 
+  },
+
   telegram: {
     botToken: process.env.TELEGRAM_BOT_TOKEN || "",
     ownerId: process.env.TELEGRAM_OWNER_ID || "",
   },
+
   mongodb: {
-    uri: process.env.MONGODB_URL, // url mongo mu
-dbName: "Cluster0",
+    uri: process.env.MONGODB_URL,
+    dbName: "Cluster0",
   },
+
   socialMedia: {
     whatsapp: "https://wa.me/6283112108527",
     telegram: "https://t.me/brockstoreidd",
     tiktok: "https://www.tiktok.com/@brockstoree",
     instagram: "https://www.instagram.com/nuradistore",
-  }
+  },
 }
